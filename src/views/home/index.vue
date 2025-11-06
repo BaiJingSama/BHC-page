d0ff40170b6e9ad9ace305bef5619a78676bdff6
gouyuchen <gouyuchen@zhiyuanhui.com>
2025-11-06 22:44:41 +0800

    feat:完成首页轮播图组件引入

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,69 @@
 <template>
-  <div class="p-8">
-    <el-button type="primary" @click="changeRoute('notice')">去公告页</el-button>
-    <el-button type="primary" @click="changeRoute('downGame')">去下载游戏</el-button>
-    <el-button type="primary" @click="changeRoute('recommendBh')">去了解BH</el-button>
-    <el-button type="primary" @click="changeRoute('recommendIntegration')">去积分系统</el-button>
-    <el-button type="primary" @click="changeRoute('recommendGame')">去游戏介绍</el-button>
-    <h1 class="text-4xl font-bold mt-4">Vue 3 + Element Plus + TailwindCSS</h1>
-    <div class="text-blue-500 text-xl font-bold">Tailwind 生效了！</div>
-    <div class="text-base md:text-lg lg:text-xl">
-      响应式文字大小
-    </div>
+  <div class="w-full">
+    <el-carousel arrow="always" :interval="3000" :height="carouselHeight">
+      <el-carousel-item v-for="(item, index) in carouselList" :key="index">
+        <img ref="carouselImgs" @load="onImgLoad(index)" :src="item" alt="" class="w-full h-auto block">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useRouter } from 'vue-router'
-const router = useRouter()
-const changeRoute = (path: string) => {
-  router.push(`/${path}`)
+import { ref, nextTick } from 'vue'
+import carouselImage1 from './images/carousel-1.png'
+// import { useRouter } from 'vue-router'
+// const router = useRouter()
+// const changeRoute = (path: string) => {
+//   router.push(`/${path}`)
+// }
+
+
+const carouselList = ref([
+  carouselImage1,
+  carouselImage1,
+  carouselImage1
+])
+
+const carouselHeight = ref('0px')
+
+// 保存图片 refs
+const carouselImgs = ref([])
+
+const onImgLoad = async (index) => {
+  // 只在第一张图片加载后设置高度（避免多次抖动）
+  if (carouselHeight.value !== '0px') return
+
+  await nextTick()
+
+  const img = carouselImgs.value[index]
+  if (!img) return
+
+  const height = img.clientHeight
+  carouselHeight.value = height + 'px'
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+::v-deep .el-carousel__container {
+  z-index: 1;
+}
+
+::v-deep .el-carousel__arrow {
+  background-color: transparent !important;
+  top: 65% !important;
+
+  i svg {
+    color: #b15cff;
+    filter:
+      drop-shadow(0 0 4px #ffffff) drop-shadow(0 0 20px #b15cff) drop-shadow(0 0 40px #b15cff) drop-shadow(0 0 60px #b15cff);
+  }
+}
+
+::v-deep .el-carousel__arrow--right {
+  right: 10px !important;
+}
+
+::v-deep .el-carousel__arrow--left {
+  left: 10px !important;
+}
+</style>

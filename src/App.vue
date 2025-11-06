3595f1b03b757aab6c8feeee6fa34fd976e0bb5b
gouyuchen <gouyuchen@zhiyuanhui.com>
2025-11-06 22:44:29 +0800

    feat:完成header全局组件样式

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
-<script setup>
-
-</script>
-
 <template>
-  <router-view />
+  <div class="relative">
+    <HeaderComponent />
+
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import HeaderComponent from './components/header/index.vue'
+</script>
+
---
+++ src/components/header/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex justify-between items-center px-8 py-6 absolute inset-x-0 top-0 z-2">
+    <img src="./images/header-logo.png" alt="" class="shrink-0 w-72 max-w-[34vw] h-auto" />
+    <div class="flex items-center gap-4">
+      <el-dropdown trigger="click" effect="dark" :show-arrow="false">
+        <button class="flex items-center bg-[#ab60ff] text-white rounded-full px-2 py-1 font-medium text-sm">
+            <img src="./images/header-button-icon.png" alt="" class="mr-1 w-3 h-3" style="font-size:inherit;" />
+            <span class="w-15 text-center">{{ language }}</span>
+        </button>
+        <template #dropdown>
+          <el-dropdown-menu class="dropdown-menu">
+            <el-dropdown-item :icon="Plus" @click="()=>language = '简体中文'">简体中文</el-dropdown-item>
+            <el-dropdown-item :icon="Plus" @click="()=>language = 'English'">English</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+      <img src="./images/header-more-icon.png" alt="" class="w-4 h-4" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+const language = ref('简体中文')
+</script>
+
+<style lang="scss" scoped>
+.dropdown-menu{
+  background-color: #230a45 !important;
+  padding: 0;
+  border: none !important;
+  ::v-deep .el-dropdown-menu__item{
+    color: #fff !important;
+    padding: 5px 12px;
+  }
+}
+</style>

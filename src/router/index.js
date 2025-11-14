2725a2904a495f81327ca6e07310662a66779f24
gouyuchen <gouyuchen@zhiyuanhui.com>
2025-11-14 18:09:17 +0800

    fix:修复bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import Home from '../views/home/index.vue'
 import Notice from '../views/notice/index.vue'
 import RecommendBh from '../views/recommendBh/index.vue'
